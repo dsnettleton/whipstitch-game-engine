41289c158ffc71b77a38d86d809d32342e4623e6
D. Scott Nettleton <dsnettleton@gmail.com>
2013-02-16 18:33:42 -0500

    Intermittent commit; beginning HUD interface

---
+++ wsAssets/wsPanel.h
@@ -0,0 +1,55 @@
+/**
+ *  wsPanel.h
+ *  Jan 30, 2013
+ *  D. Scott Nettleton
+ *
+ *  This file defines the class wsPanel, which is an image drawn
+ *  directly to the screen (i.e., a HUD element). Panels can contain
+ *  many other interface elements, such as buttons, lists, progressbars,
+ *  textboxes, onscreen text, etc.
+ *
+ *  HUD coordinates assume a screen size of 1600x900 for convenience.
+ *  When the game is run, if the display is not widescreen, coordinates
+ *  are clipped from both ends. So to support, for example, a 4:3 screen
+ *  ratio, it's best either to keep all elements within the range x=[200, 1200],
+ *  or to rearrange the elements when the screen is resized.
+ *
+ *  Copyright D. Scott Nettleton, 2013
+ *  This software is released under the terms of the
+ *  Lesser GNU Public License (LGPL).
+ *  This file is part of the Whipstitch Game Engine.
+ *  The Whipstitch Game Engine is free software: you can redistribute it
+ *  and/or modify it under the terms of the GNU Lesser Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *  The Whipstitch Game Engine is distributed in the hope that it will be
+ *  useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser Public License for more details.
+ *  You should have received a copy of the GNU Lesser Public License
+ *  along with The Whipstitch Game Engine.
+ *  If not, see <http://www.gnu.org/licenses/>.
+*/
+#ifndef WS_PANEL_H_
+#define WS_PANEL_H_
+
+#include "wsPanelElement.h"
+
+class wsPanel : wsPanelElement {
+  private:
+    //  Private Data Members
+    #if WS_GRAPHICS_BACKEND == WS_BACKEND_OPENGL
+        wsIndexArray* indexArrays;
+        u32 numIndexArrays;
+        u32 vertexArray;
+    #endif
+  public:
+    //  Constructors and Deconstructors
+    wsPanel(vec4 myRectangle, u32 myLayer, u32 myProperties, const char* myColorMap= WS_NULL) {}
+    //  Setters and Getters
+    //  Go Here
+    //  Operational Methods
+    //  Go Here
+};
+
+#endif //  WS_PANEL_H_
---
+++ wsAssets/wsPanelElement.h
@@ -0,0 +1,75 @@
+/**
+ *  wsPanelElement.h
+ *  Jan 30, 2013
+ *  D. Scott Nettleton
+ *
+ *  This file declares the abstract base class wsPanelElement.
+ *  A panel element is a HUD interface object which is drawn onto
+ *  a wsPanel object. A wsPanel itself is a Panel Element, so one
+ *  panel may contain many.
+ *
+ *  Copyright D. Scott Nettleton, 2013
+ *  This software is released under the terms of the
+ *  Lesser GNU Public License (LGPL).
+ *  This file is part of the Whipstitch Game Engine.
+ *  The Whipstitch Game Engine is free software: you can redistribute it
+ *  and/or modify it under the terms of the GNU Lesser Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *  The Whipstitch Game Engine is distributed in the hope that it will be
+ *  useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser Public License for more details.
+ *  You should have received a copy of the GNU Lesser Public License
+ *  along with The Whipstitch Game Engine.
+ *  If not, see <http://www.gnu.org/licenses/>.
+*/
+#ifndef WS_PANEL_ELEMENT_H_
+#define WS_PANEL_ELEMENT_H_
+
+#include "wsRenderSystem.h"
+
+#define WS_HUD_VISIBLE      0x00000001  //  Whether to draw this element
+#define WS_HUD_PASSABLE     0x00000002  //  Whether this element is interactive
+#define WS_HUD_FOCUS        0x00000004  //  Whether this element is the active input element
+#define WS_HUD_MOUSE_OVER   0x00000008  //  Whether the mouse pointer is currently over this element
+#define WS_HUD_HAS_IMAGE    0x00000010  //  Whether this element has a colormap
+
+class wsPanelElement {
+  protected:
+    //  Protected Data Members
+    vec4 rectangle;
+    u32 layer;  //  Panels with higher layers are drawn on top of other panels
+    u32 properties; //  HUD properties
+    u32 colorMap; //  Texture id
+  public:
+    //  Constructors and Deconstructors
+    wsPanelElement(vec4 myRectangle, u32 myLayer, u32 myProperties, const char* myColorMap= WS_NULL) :
+                    rectangle(myRectangle), layer(myLayer), properties(myProperties), colorMap(myColorMap) {
+      if (myColorMap != WS_NULL) {
+        char filepath[264] = { "textures/" };
+        strcat(filepath, myColorMap);
+        wsRenderer.loadTexture(&colorMap, filepath);
+      }
+    }
+    //  Setters and Getters
+    void disable(u32 property) { property ^= properties; properties &= property; }
+    void enable(u32 property) { properties |= property; }
+    f32 getHeight() { return rectangle.rectH; }
+    u32 getLayer() { return layer; }
+    u32 getProperties() { return properties; }
+    const vec4& getRect() { return rect; }
+    f32 getWidth() { return rectangle.rectW; }
+    f32 getX() { return rectangle.x; }
+    f32 getY() { return rectangle.y; }
+    bool hasProperty(u32 property) { return (properties & property); }
+    void setHeight(f32 my) { rectangle.rectH = my; }
+    void setLayer(u32 my) { layer = my; }
+    void setPos(f32 myX, f32 myY) { rectangle.x = myX; rectangle.y = myY; }
+    void setRect(const vec4& myRect) { rectangle = myRect; }
+    void setWidth(f32 my) { rectangle.rectW = my; }
+    void setX(f32 my) { rectangle.x = my; }
+    void setY(f32 my) { rectangle.y = my; }
+};
+
+#endif //  WS_PANEL_ELEMENT_H_
